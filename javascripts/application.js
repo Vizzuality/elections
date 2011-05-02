782d020ad031ef1b526cbd9fbdd62a68b2b5905a
Javier Álvarez Medina <xavijam.xavijam@gmail.com>
2011-05-02 23:11:38 +0200

    new controls

---
+++ javascripts/application.js
@@ -0,0 +1,25 @@
+
+  jQuery(function($) {
+    
+    //Control tab menu - map or graph
+    $('div#tab_menu a').click(function(ev){
+      ev.stopPropagation();
+      ev.preventDefault();
+      var className = ($(this).hasClass('map'))?'map':'graph';
+      if (!$(this).hasClass('selected')) {
+        $('div#tab_menu a').removeClass('selected');
+        if (className=='map') {
+          $('div#map').css('zIndex',10);
+          $('div#graph').css('zIndex',0);
+        } else {
+          $('div#map').css('zIndex',0);
+          $('div#graph').css('zIndex',10);
+        }
+        $(this).addClass('selected')
+      }
+    });
+    
+    //initialize map and map modules
+    initializeMaps();
+  });
+  
---
+++ javascripts/maps/Geocoder.js
@@ -0,0 +1,37 @@
+    
+    var geocoder = new google.maps.Geocoder();
+
+    // Search map module with dom bind events
+    function initializeSearch() {    
+      
+      $('form.search input.text').focusin(function(){
+        var value = $(this).val();
+        if (value=="Busca un lugar...") {
+          $(this).val('');
+        }
+      });
+      
+      $('form.search input.text').focusout(function(){
+        var value = $(this).val();
+        if (value=="") {
+          $(this).val('Busca un lugar...');
+        }
+      });
+      
+      $('form.search').submit(function(ev){
+        ev.stopPropagation();
+        ev.preventDefault();
+        searchLocation($(this).find('input.text').val());
+      });
+    }
+    
+    
+    function searchLocation(location) {
+      geocoder.geocode( { 'address': location}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          peninsula.fitBounds(results[0].geometry.bounds);
+        } else {
+          alert('not found');
+        }
+      });
+    }
