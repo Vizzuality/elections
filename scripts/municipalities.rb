08e8ab8f2fbc9ef8bfed4ae42ca5c494cb1bfbe7
Fernando Blat <ferblape@gmail.com>
2011-05-04 18:14:34 +0200

    Run municipalities very fast!

--- scripts/municipalities.rb
+++ scripts/municipalities.rb
@@ -7,7 +7,7 @@ psoe_id, pp_id = get_psoe_pp_id
 processes      = get_processes
 autonomies     = get_autonomies
 provinces      = get_provinces
-variables      = get_variables
+variables      = get_variables(4)
 
 base_path = FileUtils.pwd
 
@@ -33,40 +33,29 @@ end
 if n
   autonomies = autonomies.in_groups(n)[part]
 end
-
-municipalities_not_found = 0
+puts 
 autonomies.each do |autonomy_hash|
   threads = []
   provinces.select{ |p| p[:id_1] == autonomy_hash[:id_1] }.each do |province|
-    threads << Thread.new(province) do |province|
+    threads << Thread.new(province) do |province|      
+      query = <<-SQL
+select #{MUNICIPALITIES_TABLE}.cartodb_id, name_4, votantes_totales, censo_total, #{MUNICIPALITIES_VOTATIONS}.gadm4_cartodb_id, 
+       proceso_electoral_id, primer_partido_id, primer_partido_percent, segundo_partido_id, segundo_partido_percent,
+       #{variables.join(',')}
+from   #{MUNICIPALITIES_TABLE}, #{MUNICIPALITIES_VOTATIONS}, vars_socioeco_x_municipio
+where #{MUNICIPALITIES_VOTATIONS}.gadm4_cartodb_id = #{MUNICIPALITIES_TABLE}.cartodb_id AND 
+      vars_socioeco_x_municipio.gadm4_cartodb_id = #{MUNICIPALITIES_VOTATIONS}.gadm4_cartodb_id AND
+      #{MUNICIPALITIES_TABLE}.id_2 = #{province[:id_2]}
+SQL
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new("/v1?sql=#{CGI.escape("select cartodb_id, id_2, id_4, name_4 from #{MUNICIPALITIES_TABLE} where id_2 = #{province[:id_2]}")}&#{oauth_token}")
+      request = Net::HTTP::Get.new("/v1?sql=#{CGI.escape(query)}&#{oauth_token}")
       response = http.request(request)
-      municipalities = JSON.parse(response.body)["rows"]
-      municipalities.each do |municipality|
-        found = true
-        query = <<-SQL
-select votantes_totales, censo_total, #{MUNICIPALITIES_VOTATIONS}.gadm4_cartodb_id, proceso_electoral_id, primer_partido_id, primer_partido_percent, segundo_partido_id, segundo_partido_percent,
-       #{variables.join(',')}
-from #{MUNICIPALITIES_VOTATIONS}, vars_socioeco_x_municipio
-where #{MUNICIPALITIES_VOTATIONS}.gadm4_cartodb_id = #{municipality['cartodb_id']} AND vars_socioeco_x_municipio.gadm4_cartodb_id = #{MUNICIPALITIES_VOTATIONS}.gadm4_cartodb_id
-SQL
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        request = Net::HTTP::Get.new("/v1?sql=#{CGI.escape(query.strip)}&#{oauth_token}")
-        response = http.request(request)
-        votes_per_municipality = JSON.parse(response.body)["rows"]
+      JSON.parse(response.body)["rows"].each do |municipality|
         variables.each do |variable|
-          next unless found
           proceso_electoral_id = processes[variable.match(/\d+/)[0].to_i]
-          unless row = votes_per_municipality.select{|h| h["gadm4_cartodb_id"] == municipality['cartodb_id'] && h["proceso_electoral_id"] == proceso_electoral_id }.first
-            puts "Not found for gadm4_cartodb_id: #{municipality['cartodb_id']} && proceso_electoral_id = #{proceso_electoral_id}"
-            found = false
-            next
-          end
+          row = municipality
           dir_path = "#{base_path}/../json/generated_data/#{variable}/autonomies/#{autonomy_hash[:name_1]}/provinces/#{province[:name_2]}/municipalities/#{municipality['name_4']}"
           FileUtils.mkdir_p(dir_path)
           if row["primer_partido_id"].to_i != psoe_id && row["primer_partido_id"].to_i != pp_id
@@ -77,6 +66,7 @@ SQL
           end
           radius = ((row["votantes_totales"].to_f / row["censo_total"].to_f) * 6000.0) / 100.0 + 20.0
           variables.each do |variable|
+            putc '.'
             json = {}
             json[municipality["name_4"]] ||= {}
             json[municipality["name_4"]]["cartodb_id"] = municipality["cartodb_id"]
@@ -94,5 +84,3 @@ SQL
   end
   threads.each{ |t| t.join }
 end
-
-puts "Not found #{municipalities_not_found} municipalities"
