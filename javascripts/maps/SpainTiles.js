21c525ed0b7f43e74b6361f82fd1b9f83327d8d9
Javier Álvarez Medina <xavijam@gmail.com>
2011-05-05 16:07:21 +0200

    new bubbles and some new variables

--- javascripts/maps/SpainTiles.js
+++ javascripts/maps/SpainTiles.js
@@ -32,51 +32,11 @@
         y = coord.y;
       }
       
-      
-      // query = "SELECT id_4 AS id,name_4 AS municipio,name_2 AS provincia,censo_total,((votantes_totales::NUMERIC / censo_total::NUMERIC) * 100)::INTEGER AS percen_participacion,primer_partido_percent,"+
-      // "pp1.name AS primer_partido_name,segundo_partido_percent,pp2.name AS segundo_partido_name,tercer_partido_percent,pp3.name AS tercer_partido_name,0 AS otros_partido_percent,center_longitude,"+
-      // "center_latitude,vsm.paro_normalizado_1996,vsm.paro_normalizado_1997,vsm.paro_normalizado_1998,vsm.paro_normalizado_1999,vsm.paro_normalizado_2000,vsm.paro_normalizado_2001,"+
-      // "vsm.paro_normalizado_2002,vsm.paro_normalizado_2003,vsm.paro_normalizado_2004,vsm.paro_normalizado_2005,vsm.paro_normalizado_2006,vsm.paro_normalizado_2007,vsm.paro_normalizado_2008,"+
-      // "vsm.paro_normalizado_2009,p_n_min_max.* FROM (SELECT max(paro_normalizado_1996) as paro_normalizado_1996_max,min(paro_normalizado_1996) as paro_normalizado_1996_min, max(paro_normalizado_1997) as paro_normalizado_1997_max,"+
-      // "min(paro_normalizado_1997) as paro_normalizado_1997_min,max(paro_normalizado_1998) as paro_normalizado_1998_max,min(paro_normalizado_1998) as paro_normalizado_1998_min,max(paro_normalizado_1999) as paro_normalizado_1999_max,"+
-      // "min(paro_normalizado_1999) as paro_normalizado_1999_min,max(paro_normalizado_2000) as paro_normalizado_2000_max,min(paro_normalizado_2000) as paro_normalizado_2000_min,max(paro_normalizado_2001) as paro_normalizado_2001_max,"+
-      // "min(paro_normalizado_2001) as paro_normalizado_2001_min,max(paro_normalizado_2002) as paro_normalizado_2002_max,min(paro_normalizado_2002) as paro_normalizado_2002_min,max(paro_normalizado_2003) as paro_normalizado_2003_max,"+
-      // "min(paro_normalizado_2003) as paro_normalizado_2003_min,max(paro_normalizado_2004) as paro_normalizado_2004_max,min(paro_normalizado_2004) as paro_normalizado_2004_min,max(paro_normalizado_2005) as paro_normalizado_2005_max,"+
-      // "min(paro_normalizado_2005) as paro_normalizado_2005_min,max(paro_normalizado_2006) as paro_normalizado_2006_max,min(paro_normalizado_2006) as paro_normalizado_2006_min,max(paro_normalizado_2007) as paro_normalizado_2007_max,"+
-      // "min(paro_normalizado_2007) as paro_normalizado_2007_min,max(paro_normalizado_2008) as paro_normalizado_2008_max,min(paro_normalizado_2008) as paro_normalizado_2008_min,max(paro_normalizado_2009) as paro_normalizado_2009_max,"+
-      // "min(paro_normalizado_2009) as paro_normalizado_2009_min FROM vars_socioeco_x_municipio) AS p_n_min_max,gadm4 AS g INNER JOIN votaciones_por_municipio AS v ON g.cartodb_id = v.gadm4_cartodb_id "+
-      // "INNER JOIN vars_socioeco_x_municipio AS vsm ON vsm.gadm4_cartodb_id = v.gadm4_cartodb_id INNER JOIN partidos_politicos AS pp1 ON pp1.cartodb_id = v.primer_partido_id "+
-      // "INNER JOIN partidos_politicos AS pp2 ON pp2.cartodb_id = v.segundo_partido_id INNER JOIN partidos_politicos AS pp3 ON pp3.cartodb_id = v.tercer_partido_id "+ 
-      // "WHERE v_get_tile("+Math.floor(x)+","+Math.floor(y)+",11) && centre_geom_webmercator and proceso_electoral_id=73";
-      
-      
-    
-      query = "SELECT id_4 AS id,name_4 AS municipio,name_2 AS provincia,censo_total,((votantes_totales::NUMERIC / censo_total::NUMERIC) * 100)::INTEGER AS percen_participacion,primer_partido_percent,"+
-      "pp1.name AS primer_partido_name,segundo_partido_percent,pp2.name AS segundo_partido_name,tercer_partido_percent,pp3.name AS tercer_partido_name,"+
-      "0 AS otros_partido_percent,center_longitude,center_latitude,vsm.paro_normalizado_1996, vsm.paro_normalizado_1997, vsm.paro_normalizado_1998, vsm.paro_normalizado_1999, vsm.paro_normalizado_2000, vsm.paro_normalizado_2001,"+
-      "vsm.paro_normalizado_2002, vsm.paro_normalizado_2003, vsm.paro_normalizado_2004, vsm.paro_normalizado_2005, vsm.paro_normalizado_2006, vsm.paro_normalizado_2007, vsm.paro_normalizado_2008, vsm.paro_normalizado_2009 "+
-      "FROM gadm4 AS g INNER JOIN votaciones_por_municipio AS v ON g.cartodb_id = v.gadm4_cartodb_id INNER JOIN vars_socioeco_x_municipio AS vsm ON vsm.gadm4_cartodb_id = v.gadm4_cartodb_id "+
-      "INNER JOIN partidos_politicos AS pp1 ON pp1.cartodb_id = v.primer_partido_id INNER JOIN partidos_politicos AS pp2 ON pp2.cartodb_id = v.segundo_partido_id INNER JOIN partidos_politicos AS "+
-      "pp3 ON pp3.cartodb_id = v.tercer_partido_id where v_get_tile("+x+","+y+",11) && centre_geom_webmercator and proceso_electoral_id = '73'";
+      query = query_municipio + "WHERE v_get_tile("+Math.floor(x)+","+Math.floor(y)+",11) && centre_geom_webmercator and proceso_electoral_id=73";
     } else if (zoom>6 && zoom<=9) {
-      query = "SELECT id_2 AS id,name_2 AS municipio,censo_total,((votantes_totales::NUMERIC / censo_total::NUMERIC) * 100)::INTEGER AS percen_participacion, "+
-      "primer_partido_percent,pp1.name AS primer_partido_name,segundo_partido_percent,pp2.name AS segundo_partido_name,pp3.name AS tercer_partido_name,tercer_partido_percent,0 AS otros_partido_percent, "+
-      "center_longitude,center_latitude,vsp.paro_normalizado_1996,vsp.paro_normalizado_1997,vsp.paro_normalizado_1998,vsp.paro_normalizado_1999, "+
-      "vsp.paro_normalizado_2000,vsp.paro_normalizado_2001,vsp.paro_normalizado_2002,vsp.paro_normalizado_2003,vsp.paro_normalizado_2004,vsp.paro_normalizado_2005, "+
-      "vsp.paro_normalizado_2006,vsp.paro_normalizado_2007,vsp.paro_normalizado_2008,vsp.paro_normalizado_2009 FROM gadm2 AS g INNER JOIN votaciones_por_provincia AS v ON g.cartodb_id = v.gadm2_cartodb_id "+
-      "INNER JOIN vars_socioeco_x_provincia AS vsp ON vsp.gadm2_cartodb_id = v.gadm2_cartodb_id INNER JOIN partidos_politicos AS pp1 ON pp1.cartodb_id = v.primer_partido_id "+
-      "INNER JOIN partidos_politicos AS pp2 ON pp2.cartodb_id = v.segundo_partido_id INNER JOIN partidos_politicos AS pp3 ON pp3.cartodb_id = v.tercer_partido_id "+
-      "WHERE v_get_tile("+coord.x+","+coord.y+","+zoom+") && centre_geom_webmercator AND proceso_electoral_id = '73'";
+      query = query_provincia + " WHERE v_get_tile("+coord.x+","+coord.y+","+zoom+") && centre_geom_webmercator AND proceso_electoral_id = '73'";
     } else {
-      query = "SELECT id_1 AS id, name_1 AS municipio, censo_total, ((votantes_totales::NUMERIC / censo_total::NUMERIC) * 100)::INTEGER AS percen_participacion, "+
-      "primer_partido_percent, pp1.name AS primer_partido_name,segundo_partido_percent,pp2.name AS segundo_partido_name,tercer_partido_percent,pp3.name AS tercer_partido_name, "+
-      "0 AS otros_partido_percent,center_longitude,center_latitude,vsp.paro_normalizado_1996,vsp.paro_normalizado_1997,vsp.paro_normalizado_1998,vsp.paro_normalizado_1999, "+
-      "vsp.paro_normalizado_2000,vsp.paro_normalizado_2001,vsp.paro_normalizado_2002,vsp.paro_normalizado_2003,vsp.paro_normalizado_2004,vsp.paro_normalizado_2005,vsp.paro_normalizado_2006, "+
-      "vsp.paro_normalizado_2007,vsp.paro_normalizado_2008,vsp.paro_normalizado_2009 FROM gadm1 AS g INNER JOIN votaciones_por_autonomia AS v ON g.cartodb_id = v.gadm1_cartodb_id  "+
-      "INNER JOIN vars_socioeco_x_autonomia AS vsp ON vsp.gadm1_cartodb_id = v.gadm1_cartodb_id INNER JOIN partidos_politicos AS pp1 ON pp1.cartodb_id = v.primer_partido_id  "+
-      "INNER JOIN partidos_politicos AS pp2 ON pp2.cartodb_id = v.segundo_partido_id INNER JOIN partidos_politicos AS pp3 ON pp3.cartodb_id = v.tercer_partido_id  "+
-      "WHERE v_get_tile("+coord.x+","+coord.y+","+zoom+") && centre_geom_webmercator AND proceso_electoral_id = '73'";
-
+      query = query_comunidad + " WHERE v_get_tile("+coord.x+","+coord.y+","+zoom+") && centre_geom_webmercator AND proceso_electoral_id = '73'";
     }
 
     // Call service
@@ -137,15 +97,20 @@
     var left = pixelCoordinate.x - tileCoordinate.x;
     var top = pixelCoordinate.y - tileCoordinate.y;
 
-
+    var less = Math.floor(point[normalization[compare]+'_'+year+'_min']);
+    var desv = (Math.ceil(point[normalization[compare]+'_'+year+'_max'])-Math.floor(point[normalization[compare]+'_'+year+'_min']))/5;
+    var value = point[normalization[compare]+'_'+year];
+    
+    console.log(less,desv,value);
+    
     var radius;
-    if (point.percen_participacion>=0 && point.percen_participacion<20) {
+    if ((less+(desv*0))>=value && value<(less+(desv*1))) {
       radius=7;
-    } else if (point.percen_participacion>=20 && point.percen_participacion<40) {
+    } else if ((less+(desv*1))>=value && value<(less+(desv*2))) {
       radius=10;
-    } else if (point.percen_participacion>=40 && point.percen_participacion<60) {
+    } else if ((less+(desv*2))>=value && value<(less+(desv*3))) {
       radius=13;
-    } else if (point.percen_participacion>=60 && point.percen_participacion<80) {
+    } else if ((less+(desv*3))>=value && value<(less+(desv*4))) {
       radius=16;
     } else {
       radius=19;
