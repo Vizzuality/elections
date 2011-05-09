9125abb995680db47eb8f46850b792d9378dbf26
saleiva <saleiva@gmail.com>
2011-05-09 16:48:10 +0200

    graph tooltip

--- javascripts/graph/graph.js
+++ javascripts/graph/graph.js
@@ -16,76 +16,77 @@
         mouseleave: function () {
           $(this).css("backgroundColor",valuesHash[$(this).parent().attr('id')]["color"]);
         },
-        click: function() {
+        dblclick: function(){
           var url = valuesHash[$(this).parent().attr('id')]["children_json_url"];
           if (url == null) {
             return false;
           } else {
-            // goDeeper(url);
-            $("#graph_infowindow").find(".top").find("h2").empty();
-            $("#graph_infowindow").find(".top").find("h2").append($(this).parent().attr('id'));
-            $("#graph_infowindow").find(".top").find(".province").empty();
-            $("#graph_infowindow").find(".top").find(".province").append(valuesHash[$(this).parent().attr('id')]["censo_total"] + " habitantes");
-            $("#graph_infowindow").find(".top").find(".stats").find("h4").empty();
-            $("#graph_infowindow").find(".top").find(".stats").find("h4").append(Math.floor(parseInt(valuesHash[$(this).parent().attr('id')]["porcentaje_participacion"])) + "% de participación");
-            
-            // First political party
-            var partido_1 = valuesHash[$(this).parent().attr('id')]["partido_1"][0].toLowerCase();
-            c1 = $('div#graph_infowindow div.top div.stats div.partido:eq(0)').attr('class').split(" ");
-            $.each(c1, function(c){
-              if(c1[c] != "partido"){
-                $('div#graph_infowindow div.top div.stats div.partido:eq(0)').removeClass(c1[c]);
-              }
-            })
-            if (partido_1=='psoe' || partido_1=="pp" || partido_1 == "iu") {
-              $('div#graph_infowindow div.top div.stats div.partido:eq(0)').addClass(partido_1);
-            } else {
-              $('div#graph_infowindow div.top div.stats div.partido:eq(0)').addClass('par1');
-            }
-            bar_width = (valuesHash[$(this).parent().attr('id')]["partido_1"][2]*175)/100;
-            $('div#graph_infowindow div.top div.stats div.partido:eq(0) span').width((bar_width<2)?2:bar_width);
-            $('div#graph_infowindow div.top div.stats div.partido:eq(0) p').text(valuesHash[$(this).parent().attr('id')]["partido_1"][0]+' ('+(valuesHash[$(this).parent().attr('id')]["partido_1"][2]*175)/100+'%)');
-
-            // Second political party
-            var partido_2 = valuesHash[$(this).parent().attr('id')]["partido_2"][0].toLowerCase();
-            c2 = $('div#graph_infowindow div.top div.stats div.partido:eq(1)').attr('class').split(" ");
-            $.each(c2, function(c){
-              if(c2[c] != "partido"){
-                $('div#graph_infowindow div.top div.stats div.partido:eq(1)').removeClass(c2[c]);
-              }
-            })
-            if (partido_2=='psoe' || partido_2=="pp" || partido_2 == "iu") {
-              $('div#graph_infowindow div.top div.stats div.partido:eq(1)').addClass(partido_2);
-            } else {
-              $('div#graph_infowindow div.top div.stats div.partido:eq(1)').addClass('par2');
-            }
-            bar_width = (valuesHash[$(this).parent().attr('id')]["partido_2"][2]*175)/100;
-            $('div#graph_infowindow div.top div.stats div.partido:eq(1) span').width((bar_width<2)?2:bar_width);
-            $('div#graph_infowindow div.top div.stats div.partido:eq(1) p').text(valuesHash[$(this).parent().attr('id')]["partido_2"][0]+' ('+(valuesHash[$(this).parent().attr('id')]["partido_2"][2]*175)/100+'%)');
-            
-            // Third political party
-            var partido_3 = valuesHash[$(this).parent().attr('id')]["partido_3"][0].toLowerCase();
-            c3 = $('div#graph_infowindow div.top div.stats div.partido:eq(2)').attr('class').split(" ");
-            $.each(c3, function(c){
-              if(c3[c] != "partido"){
-                $('div#graph_infowindow div.top div.stats div.partido:eq(2)').removeClass(c3[c]);
-              }
-            })
-            if (partido_3=='psoe' || partido_3=="pp" || partido_3 == "iu") {
-              $('div#graph_infowindow div.top div.stats div.partido:eq(2)').addClass(partido_3);
-            } else {
-              $('div#graph_infowindow div.top div.stats div.partido:eq(2)').addClass('par3');
-            }
-            bar_width = (valuesHash[$(this).parent().attr('id')]["partido_3"][2]*175)/100;
-            $('div#graph_infowindow div.top div.stats div.partido:eq(2) span').width((bar_width<2)?2:bar_width);
-            $('div#graph_infowindow div.top div.stats div.partido:eq(2) p').text(valuesHash[$(this).parent().attr('id')]["partido_3"][0]+' ('+(valuesHash[$(this).parent().attr('id')]["partido_3"][2]*175)/100+'%)');
-
-            // Other political party
-            bar_width = (valuesHash[$(this).parent().attr('id')]["partido_otros"][2]*175)/100;
-            $('div#infowindow div.stats div.partido:eq(3) span').width((bar_width<2)?2:bar_width);
-            $('div#infowindow div.stats div.partido:eq(3) p').text('OTROS ('+info.otros_partido_percent+'%)');
-
+            goDeeper(url);
           }
+        },
+        click: function() {
+          $("#graph_infowindow").find(".top").find("h2").empty();
+          $("#graph_infowindow").find(".top").find("h2").append($(this).parent().attr('id'));
+          $("#graph_infowindow").find(".top").find(".province").empty();
+          $("#graph_infowindow").find(".top").find(".province").append(valuesHash[$(this).parent().attr('id')]["censo_total"] + " habitantes");
+          $("#graph_infowindow").find(".top").find(".stats").find("h4").empty();
+          $("#graph_infowindow").find(".top").find(".stats").find("h4").append(Math.floor(parseInt(valuesHash[$(this).parent().attr('id')]["porcentaje_participacion"])) + "% de participación");
+          
+          // First political party
+          var partido_1 = valuesHash[$(this).parent().attr('id')]["partido_1"][0].toLowerCase();
+          c1 = $('div#graph_infowindow div.top div.stats div.partido:eq(0)').attr('class').split(" ");
+          $.each(c1, function(c){
+            if(c1[c] != "partido"){
+              $('div#graph_infowindow div.top div.stats div.partido:eq(0)').removeClass(c1[c]);
+            }
+          })
+          if (partido_1=='psoe' || partido_1=="pp" || partido_1 == "iu") {
+            $('div#graph_infowindow div.top div.stats div.partido:eq(0)').addClass(partido_1);
+          } else {
+            $('div#graph_infowindow div.top div.stats div.partido:eq(0)').addClass('par1');
+          }
+          bar_width = (valuesHash[$(this).parent().attr('id')]["partido_1"][2]*175)/100;
+          $('div#graph_infowindow div.top div.stats div.partido:eq(0) span').width((bar_width<2)?2:bar_width);
+          $('div#graph_infowindow div.top div.stats div.partido:eq(0) p').text(valuesHash[$(this).parent().attr('id')]["partido_1"][0]+' ('+(valuesHash[$(this).parent().attr('id')]["partido_1"][1]*175)/100+'%)');
+
+          // Second political party
+          var partido_2 = valuesHash[$(this).parent().attr('id')]["partido_2"][0].toLowerCase();
+          c2 = $('div#graph_infowindow div.top div.stats div.partido:eq(1)').attr('class').split(" ");
+          $.each(c2, function(c){
+            if(c2[c] != "partido"){
+              $('div#graph_infowindow div.top div.stats div.partido:eq(1)').removeClass(c2[c]);
+            }
+          })
+          if (partido_2=='psoe' || partido_2=="pp" || partido_2 == "iu") {
+            $('div#graph_infowindow div.top div.stats div.partido:eq(1)').addClass(partido_2);
+          } else {
+            $('div#graph_infowindow div.top div.stats div.partido:eq(1)').addClass('par2');
+          }
+          bar_width = (valuesHash[$(this).parent().attr('id')]["partido_2"][2]*175)/100;
+          $('div#graph_infowindow div.top div.stats div.partido:eq(1) span').width((bar_width<2)?2:bar_width);
+          $('div#graph_infowindow div.top div.stats div.partido:eq(1) p').text(valuesHash[$(this).parent().attr('id')]["partido_2"][0]+' ('+(valuesHash[$(this).parent().attr('id')]["partido_2"][1]*175)/100+'%)');
+          
+          // Third political party
+          var partido_3 = valuesHash[$(this).parent().attr('id')]["partido_3"][0].toLowerCase();
+          c3 = $('div#graph_infowindow div.top div.stats div.partido:eq(2)').attr('class').split(" ");
+          $.each(c3, function(c){
+            if(c3[c] != "partido"){
+              $('div#graph_infowindow div.top div.stats div.partido:eq(2)').removeClass(c3[c]);
+            }
+          })
+          if (partido_3=='psoe' || partido_3=="pp" || partido_3 == "iu") {
+            $('div#graph_infowindow div.top div.stats div.partido:eq(2)').addClass(partido_3);
+          } else {
+            $('div#graph_infowindow div.top div.stats div.partido:eq(2)').addClass('par3');
+          }
+          bar_width = (valuesHash[$(this).parent().attr('id')]["partido_3"][2]*175)/100;
+          $('div#graph_infowindow div.top div.stats div.partido:eq(2) span').width((bar_width<2)?2:bar_width);
+          $('div#graph_infowindow div.top div.stats div.partido:eq(2) p').text(valuesHash[$(this).parent().attr('id')]["partido_3"][0]+' ('+(valuesHash[$(this).parent().attr('id')]["partido_3"][1]*175)/100+'%)');
+
+          // Other political party
+          bar_width = (valuesHash[$(this).parent().attr('id')]["resto_partidos_percent"]*175)/100;
+          $('div#graph_infowindow div.stats div.partido:eq(3) span').width((bar_width<2)?2:bar_width);
+          $('div#graph_infowindow div.stats div.partido:eq(3) p').text('OTROS ('+valuesHash[$(this).parent().attr('id')]["resto_partidos_percent"]+'%)');
         },
         hover: function() {
           console.log($(this).parent().attr('id') + ' ('+valuesHash[$(this).parent().attr('id')]["cartodb_id"]+'): ' + valuesHash[$(this).parent().attr('id')]["x_coordinate"] + ', ' + valuesHash[$(this).parent().attr('id')]["y_coordinate"]);
@@ -117,6 +118,7 @@
     //changes the values on the graph
     function setValue(url){
       //TODO: VER SI PUEDO EVITAR LEER EL JSON (AL MENOS AL PRINCIPIO)
+      console.log("le llega el JSON "+ url);
       $.getJSON(url, function(data) {
         $.each(data, function(key, v) {          
           valuesHash[key] = v;
