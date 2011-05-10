96ed16dff24c085462810f12db3d64c0e781d6e4
Simon Tokumine <simon@japancentre.com>
2011-05-10 02:07:54 +0200

    added tile downloader

---
+++ scripts/map_tiles.rb
@@ -0,0 +1,122 @@
+#!/Users/simon/.rvm/rubies/ruby-1.9.2-p180/bin/ruby
+
+# RTVE tiler
+# ===============
+# ./rtve.rb [environment] [election_id]
+# 
+# environments: [development, production] 
+# election_id: [election id table primary keys]
+
+require 'rubygems'
+require 'pg'
+require 'typhoeus'
+require 'json'
+
+# sanity check arguments
+ENVR         = ARGV[0]
+ELECTION_ID = ARGV[1]
+if (ENVR != 'development' && ENVR != 'production') || (ELECTION_ID == nil)
+  puts "./rtve.rb [environment] [election_id]"
+  puts "environments: [development, production], election_id: [see procesos_electorales table]"
+  Process.exit!(true)
+end
+
+# set app settings and boot
+user       = 123
+setup      = {:development => {:host => 'localhost', :user => 'publicuser', :dbname => "cartodb_dev_user_#{user}_db"},
+              :production  => {:host => '10.211.14.63', :user => 'postgres', :dbname => "cartodb_user_#{user}_db"}}           
+settings   = setup[ENVR.to_sym]           
+
+# Create denomalised version of GADM4 table with votes, and party names
+sql = <<-EOS  
+DROP TABLE IF EXISTS gadm4_processed; 
+CREATE TABLE gadm4_processed AS (  
+SELECT 
+g.gid gid, 
+v.primer_partido_id,pp1.name primer_nombre,v.primer_partido_percent,v.primer_partido_votos,
+v.segundo_partido_id,pp2.name segundo_nombre,v.segundo_partido_percent,v.segundo_partido_votos,
+v.tercer_partido_id,pp3.name tercer_nombre,v.tercer_partido_percent,v.tercer_partido_votos,
+g.the_geom,
+g.the_geom_webmercator,
+CASE 
+WHEN pp1.name ='PSOE' THEN 
+  CASE 
+  WHEN v.primer_partido_percent >= 75  THEN 'red_H'
+  WHEN (v.primer_partido_percent >= 50) AND (v.primer_partido_percent < 75) THEN 'red_M' 
+  WHEN (v.primer_partido_percent >= 0) AND (v.primer_partido_percent < 50)  THEN 'red_L' 
+  END 
+WHEN pp1.name = 'PP' THEN 
+  CASE 
+  WHEN v.primer_partido_percent >= 75  THEN 'blue_H'
+  WHEN (v.primer_partido_percent >= 50) AND (v.primer_partido_percent < 75)  THEN 'blue_M' 
+  WHEN (v.primer_partido_percent >= 0) AND (v.primer_partido_percent < 50)  THEN 'blue_L'
+  END 
+ELSE 'other' 
+END as color  
+FROM gadm4 AS g 
+INNER JOIN votaciones_por_municipio AS v ON g.cartodb_id=v.gadm4_cartodb_id 
+INNER JOIN partidos_politicos AS pp1 ON pp1.cartodb_id = v.primer_partido_id  
+INNER JOIN partidos_politicos AS pp2 ON pp2.cartodb_id = v.segundo_partido_id   
+INNER JOIN partidos_politicos AS pp3 ON pp3.cartodb_id = v.tercer_partido_id 
+WHERE proceso_electoral_id=#{ELECTION_ID});
+ALTER TABLE gadm4_processed ADD PRIMARY KEY (gid); 
+CREATE INDEX gadm4_processed_the_geom_webmercator_idx ON gadm4_processed USING gist(the_geom_webmercator); 
+CREATE INDEX gadm4_processed_the_geom_idx ON gadm4_processed USING gist(the_geom);
+EOS
+
+conn = PGconn.open(settings)  
+conn.exec sql
+
+# there are 2 bounding boxes at each zoom level. one for spain, one for canaries 
+tile_extents = [
+  {:zoom => 6, :xmin => 30, :ymin => 23, :xmax => 32, :ymax => 25},
+  {:zoom => 6, :xmin => 28, :ymin => 26, :xmax => 29, :ymax => 27},
+  {:zoom => 7, :xmin => 60, :ymin => 46, :xmax => 65, :ymax => 50},
+  {:zoom => 7, :xmin => 57, :ymin => 52, :xmax => 59, :ymax => 54},
+  {:zoom => 8, :xmin => 120, :ymin => 92, :xmax => 131, :ymax => 101},
+  {:zoom => 8, :xmin => 114, :ymin => 105, :xmax => 118, :ymax => 108},
+  {:zoom => 9, :xmin => 241, :ymin => 185, :xmax => 263, :ymax => 203},
+  {:zoom => 9, :xmin => 229, :ymin => 211, :xmax => 237, :ymax => 216},
+  {:zoom => 10, :xmin => 482, :ymin => 370, :xmax => 526, :ymax => 407},
+  {:zoom => 10, :xmin => 458, :ymin => 422, :xmax => 475, :ymax => 433},
+  {:zoom => 11, :xmin => 964, :ymin => 741, :xmax => 1052, :ymax => 815},
+  {:zoom => 11, :xmin => 916, :ymin => 844, :xmax => 951, :ymax => 866},
+  {:zoom => 12, :xmin => 1929, :ymin => 1483, :xmax => 2105, :ymax => 1631},
+  {:zoom => 12, :xmin => 1832, :ymin => 1688, :xmax => 1902, :ymax => 1732},  
+] 
+
+tile_url    = "http://ec2-50-16-103-51.compute-1.amazonaws.com/tiles"
+hydra       = Typhoeus::Hydra.new(:max_concurrency => 200)
+time_start  = Time.now
+start_tiles = 0
+total_tiles = tile_extents.inject(0) do |sum, extent|
+  sum += ((extent[:xmax] - extent[:xmin] + 1) * (extent[:ymax] - extent[:ymin] + 1))
+  sum
+end  
+
+tile_extents.each do |extent|
+  (extent[:xmin]..extent[:xmax]).to_a.each do |x|
+    (extent[:ymin]..extent[:ymax]).to_a.each do |y|
+      file_name = "#{x}_#{y}_#{extent[:zoom]}_#{ELECTION_ID}.png"
+      if File.exists? "images/#{file_name}"
+        total_tiles -= 1
+      else  
+        file_url  = "#{tile_url}/#{x}/#{y}/#{extent[:zoom]}/users/#{user}/layers/gadm1%7Cgadm2%7Cgadm3%7Cgadm4_processed"
+        tile_request = Typhoeus::Request.new(file_url)
+        tile_request.on_complete do |response|
+          start_tiles += 1
+          puts "#{start_tiles}/#{total_tiles}: downloaded #{file_name}"
+          File.open("images/#{file_name}", "w+") do|f|
+            f.write response.body
+          end
+        end
+        hydra.queue tile_request  
+      end  
+    end
+  end    
+end
+
+hydra.run
+time_end = Time.now
+secs = time_end - time_start
+puts "Total time: #{sprintf("%.2f", secs)} seconds (#{sprintf("%.2f", secs/60.0)} mins). #{sprintf("%.2f", total_tiles/secs)} tiles per second."
