e188f6b9aa0c5482706fa827c56e7bf183edda32
Javier Arce <javierarce@gmail.com>
2011-05-11 16:09:44 +0200

    Added hash with the content of the infoTooltip

--- javascripts/utils.js
+++ javascripts/utils.js
@@ -1,25 +1,37 @@
+var normalization = {
+  'paro': 'paro_normalizado'
+};
 
-  var normalization = {
-    'paro': 'paro_normalizado'
+var custom_map_style = [{featureType:"administrative.country",elementType:"all",stylers:[{saturation:-100},{visibility:"off"}]},{featureType:"administrative.province",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"administrative.locality",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"road.arterial",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"all",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"all",elementType:"all",stylers:[{lightness:7},{saturation:-91}]},{featureType:"all",elementType:"all",stylers:[]}];
+
+var tooltipInfo = {
+  "PIB per cápita" : {
+    content: "Desviación respecto a la media de edad de cada municipio",
+    left: "más mayores",
+    right: "más jóvenes"
+  },
+  "Tasa de paro" : {
+    content: "Desviación respecto al número de parados.",
+    left: "más parados",
+    right: "menos parados"
   }
-  
-  var custom_map_style = [{featureType:"administrative.country",elementType:"all",stylers:[{saturation:-100},{visibility:"off"}]},{featureType:"administrative.province",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"administrative.locality",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"road.arterial",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"all",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"all",elementType:"all",stylers:[{lightness:7},{saturation:-91}]},{featureType:"all",elementType:"all",stylers:[]}];
-  
-  var table_zoom = {
-    12:'gadm4',
-    11:'gadm4',
-    9:'gadm2',
-    8:'gadm2',
-    7:'gadm2',
-    6:'gadm1'
-  }
-  
-  procesos_electorales = {
-    "1987":"68","1988":"68","1989":"68","1990":"68",
-    "1991":"69","1992":"69","1993":"69","1994":"69",
-    "1995":"70","1996":"70","1997":"70","1998":"70",
-    "1999":"71","2000":"71","2001":"71","2002":"71",
-    "2003":"72","2004":"72","2005":"72","2006":"72",
-    "2007":"73","2008":"73","2009":"73","2010":"73",
-    "2011":"74"
-  };
+};
+
+var table_zoom = {
+  12:'gadm4',
+  11:'gadm4',
+  9:'gadm2',
+  8:'gadm2',
+  7:'gadm2',
+  6:'gadm1'
+};
+
+procesos_electorales = {
+  "1987":"68","1988":"68","1989":"68","1990":"68",
+  "1991":"69","1992":"69","1993":"69","1994":"69",
+  "1995":"70","1996":"70","1997":"70","1998":"70",
+  "1999":"71","2000":"71","2001":"71","2002":"71",
+  "2003":"72","2004":"72","2005":"72","2006":"72",
+  "2007":"73","2008":"73","2009":"73","2010":"73",
+  "2011":"74"
+};
