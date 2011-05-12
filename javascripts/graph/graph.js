7e92c31c8a1c2f9a402968b42fd408ecffd3fc27
Javier Arce <javierarce@vizzuality.com>
2011-05-12 14:06:25 +0200

    Fixed infoWindow positioning and background size

--- javascripts/graph/graph.js
+++ javascripts/graph/graph.js
@@ -1,18 +1,18 @@
-    
+
     //Vars determining the center of the graph
     var offsetScreenX = 510;
-    var offsetScreenY = 265;  
+    var offsetScreenY = 265;
 
     var graph_bubble_index = 100;
     var valuesHash = {};
     var nBubbles = 0;
-    
+
     var graphLegend,graphBubbleInfowindow;
 
     jQuery.easing.def = "easeInOutCubic";
-    
+
     function initializeGraph() {
-      
+
       $(".innerBubble").live({
         mouseenter: function () {
           var radius = $(this).height()/2;
@@ -30,7 +30,7 @@
         dblclick: function(){
           graphBubbleTooltip.hide();
           graphBubbleInfowindow.hide();
-          
+
           var url = valuesHash[$(this).parent().attr('id')]["children_json_url"];
           if (url == null) {
             return false;
@@ -40,18 +40,18 @@
         },
         click: function() {
           var radius = $(this).height()/2;
-          var top = parseInt($(this).parent().css('top').replace('px','')) - 100;
-          var left = parseInt($(this).parent().css('left').replace('px','')) + 90;
+          var top  = $(this).parent().offset().top - 260;
+          var left = $(this).parent().offset().left - 118;
           graphBubbleTooltip.hide();
           graphBubbleInfowindow.change(left,top,$(this).parent().attr('id'));
         },
       });
-      
-      
-      
+
+
+
       // Bubble graph infowindow
       graphBubbleInfowindow = (function() {
-        
+
         //Create infowindow and add it to DOM
         $('body').append(
           '<div class="infowindow" id="graph_infowindow" style="z-index:1000">'+
@@ -73,10 +73,10 @@
           '    <a class="compare">Comparar</a>'+
           '  </div>'+
           '</div>');
-          
+
         $('div#graph_infowindow a.close_infowindow').click(function(ev){ev.stopPropagation();ev.preventDefault(); hideInfowindow()});
-        
-        
+
+
         function showInfowindow(left,top) {
           $('div#graph_infowindow').css({opacity:0,visibility:'visible',left:left+'px',top:top+'px'});
           $('div#graph_infowindow').stop().animate({
@@ -84,8 +84,8 @@
             opacity: 1
           }, 250, 'swing');
         }
-        
-        
+
+
         function hideInfowindow() {
           $('div#graph_infowindow').stop().animate({
             top: '+=' + 10 + 'px',
@@ -94,13 +94,13 @@
       			$('div#graph_infowindow').css({visibility:"hidden"});
       		});
         }
-        
-        
+
+
         function changeData(left,top,data_id) {
           if ($("#graph_infowindow").attr('alt')==data_id && $("#graph_infowindow").is(':visible')) {
             return false;
           }
-          
+
           $("#graph_infowindow").attr('alt',data_id);
           $("#graph_infowindow").find(".top").find("h2").empty();
           $("#graph_infowindow").find(".top").find("h2").append(data_id);
@@ -108,7 +108,7 @@
           $("#graph_infowindow").find(".top").find(".province").append(valuesHash[data_id]["censo_total"] + " habitantes");
           $("#graph_infowindow").find(".top").find(".stats").find("h4").empty();
           $("#graph_infowindow").find(".top").find(".stats").find("h4").append(Math.floor(parseInt(valuesHash[data_id]["porcentaje_participacion"])) + "% de participación");
-          
+
           // First political party
           var partido_1 = valuesHash[data_id]["partido_1"][0].toLowerCase();
           c1 = $('div#graph_infowindow div.top div.stats div.partido:eq(0)').attr('class').split(" ");
@@ -142,7 +142,7 @@
           bar_width = (valuesHash[data_id]["partido_2"][2]*175)/100;
           $('div#graph_infowindow div.top div.stats div.partido:eq(1) span').width((bar_width<2)?2:bar_width);
           $('div#graph_infowindow div.top div.stats div.partido:eq(1) p').text(valuesHash[data_id]["partido_2"][0]+' ('+(valuesHash[data_id]["partido_2"][1]*175)/100+'%)');
-          
+
           // Third political party
           var partido_3 = valuesHash[data_id]["partido_3"][0].toLowerCase();
           c3 = $('div#graph_infowindow div.top div.stats div.partido:eq(2)').attr('class').split(" ");
@@ -164,25 +164,25 @@
           bar_width = (valuesHash[data_id]["resto_partidos_percent"]*175)/100;
           $('div#graph_infowindow div.stats div.partido:eq(3) span').width((bar_width<2)?2:bar_width);
           $('div#graph_infowindow div.stats div.partido:eq(3) p').text('OTROS ('+valuesHash[data_id]["resto_partidos_percent"]+'%)');
-          
+
           showInfowindow(left,top);
         }
-        
+
         return {
           show: showInfowindow,
           hide: hideInfowindow,
           change: changeData
         }
-      }()); 
-      
-      
-      
-      
+      }());
+
+
+
+
       // Tooltip when mouseover some bubble
       var graphBubbleTooltip = (function() {
         // Create the element - add it to DOM
     	  $('div#graph_container').append('<p class="graph_bubble_tooltip">Comunidad de Madrid</p>');
-    	  
+
     	  function hideTooltip() {
           $('p.graph_bubble_tooltip').hide();
     	  }
@@ -192,7 +192,7 @@
     	    var offset = $('p.graph_bubble_tooltip').width()/2;
     	    $('p.graph_bubble_tooltip').css('left',left-offset+10+'px');
     	    $('p.graph_bubble_tooltip').css('top',top+'px');
-    	    
+
     	    $('p.graph_bubble_tooltip').css('zIndex',graph_bubble_index);
           $('p.graph_bubble_tooltip').show();
     	  }
@@ -202,10 +202,10 @@
     	    show: showTooltip
     	  }
     	}());
-    	
-    	
-    	
-    	
+
+
+
+
     	graphLegend = (function() {
     	  // Create the element - add it to DOM
     	  $('div#graph').append(
@@ -219,15 +219,15 @@
           '    <div class="partido otros"><div class="bar"><span style="width:20px"></span></div><p>OTROS (11%)</p></div>'+
           '  </div>'+
           '</div>');
-          
+
         function showLegend() {
           $('div.graph_legend').fadeIn();
         }
-        
+
         function hideLegend() {
           $('div.graph_legend').fadeOut();
         }
-        
+
         function changeData(results,names) {
           if (names.length!=0) {
             showLegend();
@@ -235,30 +235,30 @@
             hideLegend();
           }
         }
-    	  
+
   	    return {
           hide: hideLegend,
           show: showLegend,
           change: changeData
     	  }
     	}());
-    
-    
-    
+
+
+
       //Control resize window
       $(window).resize(function(){
         graphBubbleInfowindow.hide();
       });
     }
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
     /*GRAPH FUNCTIONS!*/
-    
+
     function restartGraph() {
       graphLegend.hide();
       graph_bubble_index = 100;
@@ -266,20 +266,20 @@
       valuesHash = {};
       createBubbles("/json/generated_data/autonomies/"+normalization[compare]+"_"+year+".json");
     }
-    
+
 
     function createBubbles(url){
       $.getJSON(url, function(data) {
-        
+
         var one = true;
         _.each(data, function(val, key) {
-          
+
           //Check data for show legend or not
           if (one) {
             graphLegend.change(data[key].parent_results, data[key].parents);
             one = false;
           }
-          
+
           valuesHash[key] = val;
           nBubbles = nBubbles+1;
           $('#graph_container').append("<div class='bubbleContainer' id='"+key+"'><div class='outerBubble'></div><div class='innerBubble'></div></div>");
@@ -287,16 +287,16 @@
           $('#'+key).css("top",(offsetScreenY).toString()+"px");
           $('#'+key).css("opacity","0");
           $('#'+key).find('.innerBubble').css("backgroundColor",val["color"]);
-          
+
           updateBubble('#'+key,offsetScreenX+parseInt(val["x_coordinate"]),offsetScreenY+parseInt(val["y_coordinate"]),val["radius"],val["color"]);
         });
-      }); 
+      });
     }
-    
-    
+
+
     function setValue(url){
       $.getJSON(url, function(data) {
-        _.each(data, function(v,key) {      
+        _.each(data, function(v,key) {
           valuesHash[key] = v;
           updateBubble('#'+key,offsetScreenX+parseInt(v["x_coordinate"]),offsetScreenY+parseInt(v["y_coordinate"]),v["radius"],v["color"]);
         });
@@ -316,20 +316,20 @@
         }, 1000, function(){
           //console.log("ukelele");
       });
-  
+
       $(bubble).find('.outerBubble').animate({
         height: val.toString() + "px",
         width: val.toString() + "px",
         top: offset.toString() + "px",
         left: offset.toString() + "px"
       }, 1000);
-  
+
       $(bubble).find('.innerBubble').animate({
         height: (val-10).toString() + "px",
         width: (val-10).toString() + "px",
         top: (offset + 5).toString() + "px",
         left: (offset + 5).toString() + "px",
-        backgroundColor: c.toString() + "px"        
+        backgroundColor: c.toString() + "px"
       }, 1000);
     }
 
@@ -361,10 +361,10 @@
         }
       );
     }
-    
-    
-    
 
-    
 
-    
+
+
+
+
+
