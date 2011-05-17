f16d6990ecfdd2028f68931fea2eab5f98385dae
Javier Álvarez Medina <xavijam@gmail.com>
2011-05-18 01:16:42 +0200

    changes on slider

--- javascripts/header.js
+++ javascripts/header.js
@@ -4,10 +4,23 @@
   var animate_interval;
   var previous_year;
   
-  var years_nodata = {}
-  
+  var years_nodata = {};
+
 
   function initializeHeader() {
+    /* Receive all the vars without data */
+    var deep;
+    if (start_zoom==6) {
+      deep = "autonomias";
+    } else if (start_zoom>6 && start_zoom<11) {
+      deep = "provincias";
+    } else {
+      deep = "municipios";
+    }
+    getUnavailableData(deep);
+
+    
+    
     // Graph - Map
     if (state == "grafico") {
       $('div#tab_menu a.map').removeClass('selected');
@@ -299,6 +312,68 @@
 
       changeHash();
     }
+  }
+  
+
+ 
+
+  function getUnavailableData(deep) {
+    $.getJSON(global_url + "/graphs/meta/" + deep + ".json", function(data) {
+      years_nodata[deep] = new Object();
+      _.each(data,function(ele,i){
+        years_nodata[deep][i] = new Array();
+        years_nodata[deep][i] = ele;
+      });
+      drawNoDataBars();
+    });
+  }
+  
+  
+  function drawNoDataBars() {
+    var deep;
+    var zoom = peninsula.getZoom();
+    if (zoom==6) {
+      deep = "autonomias";
+      if (years_nodata["autonomias"]==undefined) {
+        getUnavailableData('autonomias');
+        return false;
+      }
+    } else if (zoom>6 && zoom<11) {
+      deep = "provincias";
+      if (years_nodata["provincias"]==undefined) {
+        getUnavailableData('provincias');
+        return false;
+      }
+    } else {
+      deep = "municipios";
+      if (years_nodata["municipios"]==undefined) {
+        getUnavailableData('municipios');
+        return false;
+      }
+    }
+    
+    var left_no = years_nodata[deep][normalization[compare]][0] - 1987; 
+    var lenght_array = years_nodata[deep][normalization[compare]].length;
+    var right_no = 2011 - years_nodata[deep][normalization[compare]][lenght_array-1];
+    
+    // console.log(left_no);
+    // console.log(right_no);
+    
+    if (left_no!=0) {
+      $('span.slider_no_data_left').css({width:(left_no*4.16)+"%"});
+      $('span.slider_no_data_left').show();
+    } else {
+      $('span.slider_no_data_left').hide();
+    }
+    
+    if (right_no!=0) {
+      $('span.slider_no_data_right').css({width:(right_no*4.16)+"%"});
+      $('span.slider_no_data_right').show();
+    } else {
+      $('span.slider_no_data_right').hide();
+    }
+    
+
   }
   
   
