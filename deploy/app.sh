a0148e7d911aba3ff854527861a8ecc78d1bfec6
Vizzuality <contact@vizzuality.com>
2011-05-17 21:00:28 +0100

    ensure deplyo scripts exit properly

--- deploy/app.sh
+++ deploy/app.sh
@@ -1,5 +1,5 @@
 rm -rf /home/ubuntu/elections_deploy
 cp -R /home/ubuntu/elections /home/ubuntu/elections_deploy
 rm -rf /home/ubuntu/elections_deploy/.git
-lftp -c 'mirror -R -p --parallel=100 --no-symlinks /home/ubuntu/elections_deploy/. .' -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net
+lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -R -p --parallel=100 --no-symlinks /home/ubuntu/elections_deploy/. .' 
 rm -rf /home/ubuntu/elections_deploy
