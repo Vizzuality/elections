391b25c41edf5399267bce9153a670fdeb940e5d
Vizzuality <contact@vizzuality.com>
2011-05-17 22:37:43 +0100

    nope

--- deploy/data.sh
+++ deploy/data.sh
@@ -1,1 +1,1 @@
-lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && rm -r data && mirror -R -p --parallel=100 --no-symlinks /mnt/www/data data' 
+lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -R -p --parallel=100 --no-symlinks /mnt/www/data data' 
