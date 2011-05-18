5ef9c2020370b69ec3a68a8a88169baf4019e8ea
Vizzuality <contact@vizzuality.com>
2011-05-18 23:34:24 +0100

    updated tile uploader to only use current

--- deploy/tile_data.sh
+++ deploy/tile_data.sh
@@ -1,1 +1,1 @@
-time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 --no-symlinks --only-missing /mnt/www/data/tiles data/tiles' 
+time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 --ignore-size --ignore-size /mnt/www/data/tiles/current data/tiles/current' 
