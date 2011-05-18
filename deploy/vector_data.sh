b074b7e7e80d3d0b904aaaa9c2909abcdd4feb01
Vizzuality <contact@vizzuality.com>
2011-05-19 00:33:13 +0100

    move assets over to current symlink

--- deploy/vector_data.sh
+++ deploy/vector_data.sh
@@ -1,1 +1,1 @@
-time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 --ignore-time --ignore-size -I current/* -X tiles/ /mnt/www/data data' 
+time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 --ignore-time --ignore-size  -I current/* -X tiles/ /mnt/www/data data' 
