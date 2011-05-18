38773c01d8e2d5240f44574197cbca45dc6146e8
Vizzuality <contact@vizzuality.com>
2011-05-18 15:35:33 +0100

    test differetn sync style

--- deploy/vector_data.sh
+++ deploy/vector_data.sh
@@ -1,1 +1,1 @@
-lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -R -p --parallel=100 --no-symlinks --only-missing -X tiles/ /mnt/www/data data' 
+lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -R -p --parallel=100 --no-symlinks --only-newer -X tiles/ /mnt/www/data data' 
