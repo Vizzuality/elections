2f01810d70d66d4768081b2c97b0b10f74976732
Javier Arce <javierarce@vizzuality.com>
2011-05-18 17:38:34 +0200

    We get the correct deep level depending of the state

--- javascripts/header.js
+++ javascripts/header.js
@@ -281,8 +281,16 @@
       });
 
       function showError() {
-        var $state = (state == "mapa") ? $("#map") : $("#graph");
+        if (state == "mapa") {
+          var $state = $("#map");
+          var deep_level = getDeepLevelFromZoomLevel(peninsula.getZoom());
+        } else {
+          var $state = $("#graph");
+          var deep_level = deep;
+        }
+
         if (data_not_found != true) {
+          $state.find("div.content").html(deep_level);
           $state.find('div.fail').fadeIn("slow", function() { data_not_found = true; });
         }
       }
@@ -302,12 +310,13 @@
       }
 
       function goToNextYear() {
-        var deep_level = getDeepLevelFromZoomLevel(peninsula.getZoom());
-        updateNewSliderValue(getNextAvailableYear(deep_level));
 
         if (state == "mapa") {
+          var deep_level = getDeepLevelFromZoomLevel(peninsula.getZoom());
+          updateNewSliderValue(getNextAvailableYear(deep_level));
           failCircle.hide();
         } else {
+          updateNewSliderValue(getNextAvailableYear(deep));
           setValue(global_url + "/graphs/"+deep+"/"+graph_version+"/"+((name=="España")?'':name+'_')+normalization[compare]+"_"+year+".json");
         }
       }
