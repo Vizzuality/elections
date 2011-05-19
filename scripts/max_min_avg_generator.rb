f53a305f385a19f93059412256b30226afc79695
Fernando Espinosa <fer@ferdev.com>
2011-05-19 12:38:42 +0200

    Rounds to two decimals the max,min,avg vars jsons

--- scripts/max_min_avg_generator.rb
+++ scripts/max_min_avg_generator.rb
@@ -6,7 +6,11 @@ cartodb = get_cartodb_connection
 
 [6, 7, 11].each do |z|
   File.open("#{ENV['HOME']}/Desktop/max_min_avg_#{z}.json", 'w') do |file|
-    file.write(cartodb.query(max_min_vars_query(z)).rows.first.to_json)
+    vars = {}
+    cartodb.query(max_min_vars_query(z)).rows.first.each do |key, value|
+      vars[key] = value.to_f.round(2)
+    end
+    file.write(vars.to_json)
   end
   puts z
 end
