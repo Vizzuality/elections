da38ea50ae0411d738898f072aaac45beefbdcf5
Javier Álvarez Medina <xavijam@gmail.com>
2011-05-19 18:31:23 +0200

    infotips position changed

--- javascripts/maps/SpainTiles.js
+++ javascripts/maps/SpainTiles.js
@@ -15,7 +15,7 @@
     var div = ownerDocument.createElement('div');
     div.setAttribute('class','tile');
     //div.innerHTML= coord.x+"_"+coord.y+"_"+zoom;
-    // div.style.border = "1px solid yellow";
+    //div.style.border = "1px solid yellow";
     div.style.zIndex = 0;
     div.style.width = this.tileSize.width + 'px';
     div.style.height = this.tileSize.height + 'px';
