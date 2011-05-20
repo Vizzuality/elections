ff05d1c23d2073a18fa0f221c54b7ee0f9d4952a
Javier Arce <javierarce@vizzuality.com>
2011-05-20 16:35:52 +0200

    We set the year automatically for the default dataset (paro)

--- javascripts/graph/graph.js
+++ javascripts/graph/graph.js
@@ -32,6 +32,13 @@ function initAvailableData(deep) {
         $("div.select div.outer_select.money span.inner_select a").text(text);
         $("div.select div.option_list ul li a.paro").parent().addClass("selected");
         compare = "paro";
+
+        if (year < 2005) { // because we don't have paro information prior 2005
+          updateNewSliderValue(2005, year);
+        } else if (year > 2010) {
+          updateNewSliderValue(2010, year);
+        }
+
         changeHash();
         restartGraph();
       });
