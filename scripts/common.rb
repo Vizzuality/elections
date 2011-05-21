651bf64050ba90e7fad72b8fe3a5d4f2e7a27120
Vizzuality <contact@vizzuality.com>
2011-05-21 16:23:54 +0000

    new chart version

--- scripts/common.rb
+++ scripts/common.rb
@@ -46,7 +46,7 @@ THIRD_PARTY_COLORS = {
 }
 
 # Versions
-$graphs_next_version = "v10"
+$graphs_next_version = "v1"
 
 CartoDB::Settings = YAML.load_file('cartodb_config.yml')
 $cartodb = CartoDB::Client::Connection.new
