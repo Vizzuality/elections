072e822c09a6db330c5bb7e1040573e53eb5c674
Vizzuality <contact@vizzuality.com>
2011-05-21 23:20:35 +0100

    removed current path from vector deploy script

--- deploy/vector_data.sh
+++ deploy/vector_data.sh
@@ -1,2 +1,2 @@
-time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 -I current/* -X tiles/ /mnt/www/data data' 
+time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 -X tiles/ /mnt/www/data data' 
 
