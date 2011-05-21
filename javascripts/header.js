b724d9dd7c2d4001ce1058c65b8b07df4c58dd32
Javier Álvarez Medina <xavijam@gmail.com>
2011-05-21 17:56:52 +0200

    nan bug solved

--- javascripts/header.js
+++ javascripts/header.js
@@ -404,13 +404,14 @@
             failCircle.hide();
           } else {
             peninsula.setZoom(6);
-            var deep_level = getDeepLevelFromZoomLevel(6);
 
+            var deep_level = getDeepLevelFromZoomLevel(6);
+            
             if (years_nodata[deep_level]==undefined) {
               var count = 0;
               clearInterval(deep_interval);
               deep_interval = setInterval(function(){
-
+            
                 if (years_nodata[deep_level]==undefined) {
                   count++;
                   if (count>5) {
@@ -418,17 +419,18 @@
                   }
                 } else {
                   var next_year = getNextAvailableYear(deep_level);
-                  updateNewSliderValue(next_year);
+                  if (next_year!=year && next_year<2012) {
+                    updateNewSliderValue(next_year,year);
+                  }
                   clearInterval(deep_interval);
                 }
               },500);
             } else {
               var next_year = getNextAvailableYear(deep_level);
-              updateNewSliderValue(next_year);
+              if (next_year!=year && next_year<2012) {
+                updateNewSliderValue(next_year,year);
+              }
             }
-
-            var next_year = getNextAvailableYear(deep_level);
-            updateNewSliderValue(next_year);
           }
 
 
