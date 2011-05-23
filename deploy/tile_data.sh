566259fa0c91d9720df498cd307d433130e5e03a
Vizzuality <contact@vizzuality.com>
2011-05-23 04:21:46 +0100

    change deploy path to live for tiles

--- deploy/tile_data.sh
+++ deploy/tile_data.sh
@@ -1,1 +1,1 @@
-time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 /mnt/www/data/tiles/current data/tiles/current' 
+time lftp -c 'open -u elecciones2011,Elecc10nes rtve-004e.servidoresdns.net && mirror -RL -p --parallel=100 /mnt/www/data/tiles/live data/tiles/live' 
