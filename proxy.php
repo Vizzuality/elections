6408270afd368c95df12ac501f5d8ae8e7f627f6
Javier de la Torre <jatorre@vizzuality.com>
2011-05-24 17:06:30 +0200

    new proxy

--- proxy.php
+++ proxy.php
@@ -2,6 +2,10 @@
 $proxy_url = isset($_GET['proxy_url'])?$_GET['proxy_url']:false;
 
 @$data=file_get_contents($proxy_url);
-echo($data);
 
+if (!$data) {
+    header("HTTP/1.0 404 Not Found");
+} else {
+    echo($data);
+}
 ?>
