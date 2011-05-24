61c5e80c00a19886a3f0d7b3d746569a1e877ea5
Vizzuality <contact@vizzuality.com>
2011-05-24 02:56:44 +0100

    helper

--- scripts/common.rb
+++ scripts/common.rb
@@ -130,6 +130,10 @@ def get_x_coordinate(row, max, known_parties)
   if max == 0
     return 0
   end
+  
+  puts "row: #{row.inspect}"
+  puts "known_parties.keys.inspect: #{known_parties.keys.inspect}"
+  
   if known_parties.keys.include?(row[:primer_partido_id])
     x_coordinate = if row[:primer_partido_percent]
       if row[:primer_partido_percent] && row[:segundo_partido_percent]
